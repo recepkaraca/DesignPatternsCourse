415abd47c3c73f3c5a20fce80ab368cddf3a66c9
recepkaraca <recepkaraca@yandex.com>
2020-04-14 20:43:15 +0300

    Add object pool and prototype pattern

---
+++ object_pool/ObjectPool.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DesignPatternsCourse.ObjectPool
+{
+    public class BookPool
+    {
+        private static Lazy<BookPool> instance
+            = new Lazy<BookPool>(() => new BookPool());
+        public static BookPool Instance { get; } = instance.Value;
+
+        private const int defaultSize = 5;
+        private ConcurrentBag<Book> _bag = new ConcurrentBag<Book>();
+        private volatile int _currentSize;
+        private volatile int _counter;
+        private object _lockObject = new object();
+
+        private BookPool()
+            : this(defaultSize)
+        {
+        }
+        private BookPool(int size)
+        {
+            _currentSize = size;
+        }
+
+        public Book AcquireObject()
+        {
+            if (!_bag.TryTake(out Book item))
+            {
+                lock (_lockObject)
+                {
+                    if (item == null)
+                    {
+                        if (_counter >= _currentSize)
+                            throw new Exception();
+
+                        item = new RequestBook();
+                        _counter++;
+
+                    }
+                }
+
+            }
+
+            return item;
+        }
+
+        public void ReleaseObject(Book item)
+        {
+            _bag.Add(item);
+        }
+    }
+
+
+    public abstract class Book
+    {
+        public abstract string GatherBook();
+    }
+    internal class RequestBook : Book
+    {
+        public override string GatherBook()
+        {
+            Thread.Sleep(1000);
+            return "Gathering book with RequestBook...";
+        }
+    }
+
+
+    class ObjectPoolRun
+    {
+        public static void Main(string[] args)
+        {
+            int counter = 0;
+            Parallel.For(0, 100, (i, loopState) =>
+            {
+                Book book;
+                Exception e = null;
+                do
+                {
+                    try
+                    {
+                        book = BookPool.Instance.AcquireObject();
+                        Console.WriteLine("Thread " + Thread.CurrentThread.GetHashCode() + " : " + book.GatherBook() + " Instance id : " + book.GetHashCode());
+                        BookPool.Instance.ReleaseObject(book);
+                        e = null;
+                        counter++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Waiting...");
+                        e = ex;
+                    }
+                } while (e != null);
+            });
+
+            Console.WriteLine("Counter : " + counter);
+        }
+    }
+}
---
+++ prototype/Prototype.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsCourse.Prototype
+{
+    class Prototype
+    {
+        public static void Main(string[] args)
+        {
+            BookForShallow book1 = new BookForShallow(1, "I Robot", "Science Fiction");
+            BookForShallow book2 = (BookForShallow)book1.Clone();
+            book2.BookId = 2;
+
+            Console.WriteLine("Book 1:");
+            Console.WriteLine("ID: " + book1.BookId + " Name: " + book1.BookName + " Category: " + book1.Category);
+
+            Console.WriteLine("Book 2:");
+            Console.WriteLine("ID: " + book2.BookId + " Name: " + book2.BookName + " Category: " + book2.Category);
+
+
+            BookForDeep book3 = new BookForDeep(1, "I Robot", "Science Fiction");
+            BookForDeep book4 = (BookForDeep)book3.Clone();
+            book4.BookId = 3;
+
+            Console.WriteLine("Book 3:");
+            Console.WriteLine("ID: " + book3.BookId + " Name: " + book3.BookName + " Category: " + book3.Category);
+
+            Console.WriteLine("Book 4:");
+            Console.WriteLine("ID: " + book4.BookId + " Name: " + book4.BookName + " Category: " + book4.Category);
+        }
+    }
+
+    abstract class BookPrototype
+    {
+        public int BookId;
+        public string BookName;
+        public string Category;
+
+        public BookPrototype(int bookId, string bookName, string category)
+        {
+            BookId = bookId;
+            BookName = bookName;
+            Category = category;
+        }
+
+        public abstract BookPrototype Clone();
+    }
+
+
+    class BookForDeep : BookPrototype
+    {
+        public BookForDeep(int bookId, string bookName, string category) : base(bookId, bookName, category)
+        {
+        }
+
+        public override BookPrototype Clone()
+        {
+            return (BookPrototype)this;
+        }
+    }
+
+    class BookForShallow : BookPrototype
+    {
+        public BookForShallow(int bookId, string bookName, string category) : base(bookId, bookName, category)
+        {
+        }
+
+        // Deep Copy
+        public override BookPrototype Clone()
+        {
+            return (BookPrototype)this.MemberwiseClone();
+        }
+    }
+}
