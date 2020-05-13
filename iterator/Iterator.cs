745a8c01578c8cd09b9620a7814b44ad1dfe0ff4
recepkaraca <recepkaraca@yandex.com>
2020-05-13 03:01:08 +0300

    Add iterator and observer design patterns.

---
+++ iterator/Iterator.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsCourse.Iterator
+{
+    abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        public abstract int Key();
+
+        public abstract object Current();
+
+        public abstract bool MoveNext();
+
+        public abstract void Reset();
+    }
+
+    abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+
+    class CarIterator : Iterator
+    {
+        private CarCollection _collection;
+
+        private int _position = -1;
+
+        private bool _reverse = false;
+
+        public CarIterator(CarCollection collection, bool reverse = false)
+        {
+            this._collection = collection;
+            this._reverse = reverse;
+
+            if (reverse)
+            {
+                this._position = collection.getItems().Count;
+            }
+        }
+
+        public override object Current()
+        {
+            return this._collection.getItems()[_position];
+        }
+
+        public override int Key()
+        {
+            return this._position;
+        }
+
+        public override bool MoveNext()
+        {
+            int updatedPosition = this._position + (this._reverse ? -1 : 1);
+
+            if (updatedPosition >= 0 && updatedPosition < this._collection.getItems().Count)
+            {
+                this._position = updatedPosition;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void Reset()
+        {
+            this._position = this._reverse ? this._collection.getItems().Count - 1 : 0;
+        }
+    }
+
+    class CarCollection : IteratorAggregate
+    {
+        List<string> _collection = new List<string>();
+
+        bool _direction = false;
+
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+
+        public List<string> getItems()
+        {
+            return _collection;
+        }
+
+        public void AddItem(string item)
+        {
+            this._collection.Add(item);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new CarIterator(this, _direction);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new CarCollection();
+            collection.AddItem("BMW");
+            collection.AddItem("Mercedes");
+            collection.AddItem("Audi");
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\nReverse traversal:");
+
+            collection.ReverseDirection();
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+}
---
+++ observer/Observer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DesignPatternsCourse.Observer
+{
+    abstract class Car
+    {
+        private string _brand;
+        private double _price;
+        private List<IDealer> _dealers = new List<IDealer>();
+
+        public Car(string brand, double price)
+
+        {
+            this._brand = brand;
+            this._price = price;
+        }
+
+        public void Attach(IDealer dealer)
+        {
+            Console.WriteLine("Subject: Attached an observer.");
+            _dealers.Add(dealer);
+        }
+
+        public void Detach(IDealer dealer)
+
+        {
+            Console.WriteLine("Subject: Detached an observer.");
+            _dealers.Remove(dealer);
+        }
+        public void Notify()
+        {
+            Console.WriteLine("Subject: Notifying observers...");
+            foreach (IDealer dealer in _dealers)
+            {
+                dealer.Update(this);
+            }
+
+            Console.WriteLine("");
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    Notify();
+                }
+            }
+        }
+
+        public string Brand
+        {
+            get { return _brand; }
+        }
+    }
+
+    class Mercedes : Car { 
+        public Mercedes(string brand, double price) : base(brand, price)
+        { 
+        }
+    }
+
+    interface IDealer
+
+    {
+        void Update(Car car);
+    }
+
+    class Dealer : IDealer
+    {
+        private string _brand;
+        private Car _car;
+        public Dealer(string brand)
+        {
+            this._brand = brand;
+        }
+
+        public void Update(Car car)
+        {
+            Console.WriteLine("Notified {0} of {1}'s " + "change to {2:C}", _car, car.Brand, car.Price);
+        }
+        public Car Car
+        {
+            get { return _car; }
+            set { _car = value; }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            Mercedes mercedes = new Mercedes("Mercedes", 200000);
+            Dealer dealer1 = new Dealer("Dealer1");
+            Dealer dealer2 = new Dealer("Dealer2");
+            mercedes.Price = 200000;
+            Parallel.For(0, 5, (i, loopState) =>
+            {
+                int delay;
+                lock (rnd)
+                    delay = rnd.Next(1, 1001);
+                Thread.Sleep(delay);
+                Console.WriteLine("Thread : " + i);
+                mercedes.Attach(dealer1);
+                mercedes.Price += i;
+                mercedes.Detach(dealer1);
+            });
+
+        }
+    }
+}
